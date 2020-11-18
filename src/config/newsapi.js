430b032db3acab1595ebe2604ce114d2c7543114
John_Lamy <johnclamy@gmail.com>
2020-11-18 09:46:18 +0000

    edited newsapi to reduce duplicate code

--- src/config/newsapi.js
+++ src/config/newsapi.js
@@ -1,32 +1,33 @@
 const newsApi = {
   key: process.env.REACT_APP_NEWSAPI_KEY,
-  urls: [
+  url: 'http://newsapi.org/v2/',
+  qry: [
     {
-      top: `http://newsapi.org/v2/top-headlines?apiKey=${this.key}`,
+      top: `${this.url}top-headlines?apiKey=${this.key}`,
     },
     {
-      latest: `http://newsapi.org/v2/everything?q=health&sortBy=publishedAt&apiKey=${this.key}`,
+      latest: `${this.url}everything?q=health&sortBy=publishedAt&apiKey=${this.key}`,
     },
     {
-      uk: `http://newsapi.org/v2/top-headlines?country=uk&apiKey=${this.key}`,
+      uk: `${this.url}top-headlines?country=uk&apiKey=${this.key}`,
     },
     {
-      bbc: `http://newsapi.org/v2/top-headlines?sources=bbc&apiKey=${this.key}`,
+      bbc: `${this.url}top-headlines?sources=bbc&apiKey=${this.key}`,
     },
     {
-      biz: `http://newsapi.org/v2/everything?q=business&sortBy=publishedAt&apiKey=${this.key}`,
+      biz: `${this.url}everything?q=business&sortBy=publishedAt&apiKey=${this.key}`,
     },
     {
-      showBiz: `http://newsapi.org/v2/everything?q=entertainment&sortBy=publishedAt&apiKey=${this.key}`,
+      showBiz: `${this.url}everything?q=entertainment&sortBy=publishedAt&apiKey=${this.key}`,
     },
     {
-      health: `http://newsapi.org/v2/everything?q=health&sortBy=publishedAt&apiKey=${this.key}`,
+      health: `${this.url}everything?q=health&sortBy=publishedAt&apiKey=${this.key}`,
     },
     {
-      sport: `http://newsapi.org/v2/everything?q=sports&sortBy=publishedAt&apiKey=${this.key}`,
+      sport: `${this.url}everything?q=sports&sortBy=publishedAt&apiKey=${this.key}`,
     },
     {
-      tech: `http://newsapi.org/v2/everything?q=technology&sortBy=publishedAt&apiKey=${this.key}`,
+      tech: `${this.url}everything?q=technology&sortBy=publishedAt&apiKey=${this.key}`,
     },
   ],
 };
