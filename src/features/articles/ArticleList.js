563837181dbe84c66413258fcfde38c8d5a7497f
John_Lamy <johnclamy@gmail.com>
2021-04-27 14:28:20 +0100

    reset list file

--- src/features/articles/ArticleList.js
+++ src/features/articles/ArticleList.js
@@ -1,19 +1,3 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { ListGroup } from 'react-bootstrap';
-
-import { getArticles } from '../features/articles/articlesSlice';
-
 export default function ArticlesList() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getArticles());
-  }, [dispatch]);
-
-  return (
-    <ListGroup variant='flush'>
-      articles go here...
-    </ListGroup>
-  ) 
+  return <div>list of articles goes here...</div>;
 }
