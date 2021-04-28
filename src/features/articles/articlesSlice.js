07897f5a943ed7eeeeda92e7b0e2c09f7a7b85e8
John_Lamy <johnclamy@gmail.com>
2021-04-28 12:20:55 +0100

    set addArticle reducer

--- src/features/articles/articlesSlice.js
+++ src/features/articles/articlesSlice.js
@@ -1,15 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 import initialState from './initialState';
+import FETCH_STATUS from '../../app/helper/fetchStatus';
 
 const articlesSlice = createSlice({
   name: 'articles',
   initialState,
-  reducers: {},
+  reducers: {
+    articleAdded: {
+      reducer(state, action) {
+        state.articles.push(action.payload);
+      },
+      prepare(title, source, publishedAt, description) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            source,
+            publishedAt,
+            description,
+          },
+        };
+      },
+    },
+  },
 });
 
-// export { articleAdded } = articlesSlice.actions;
-export const selectAllArticles = (state) => state.articles;
+export const { articleAdded } = articlesSlice.actions;
+export const selectAllArticles = (state) => state.articles.articleList;
 export const selectArticleById = (state, articleId) =>
-  state.articles.find((a) => a.id === articleId);
+  state.articles.articleList.find((a) => a.id === articleId);
 export default articlesSlice.reducer;
