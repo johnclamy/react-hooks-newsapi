471b7bcf90c85dd606e855b61e27fc82df3a80a5
John_Lamy <johnclamy@gmail.com>
2021-04-28 11:34:06 +0100

    remove articles from initalState

--- src/features/articles/initialState.js
+++ src/features/articles/initialState.js
@@ -1,23 +1,9 @@
-const Articles = [
-  {
-    id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
-    source: 'The Guardian',
-    title: 'Electric cars: five best buys, from new models to used bargains',
-    description: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.`,
-    url:
-      'https://www.theguardian.com/money/2020/jul/11/electric-cars-five-best-buys-from-new-models-to-used-bargains',
-    publishedAt: '2020-07-13T10:19:00Z',
-  },
-  {
-    id: '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed',
-    source: 'TechRadar',
-    title:
-      "Canon EOS R5: 9 new things we've learnt about the full-frame mirrorless flagship",
-    description: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`,
-    url:
-      'https://www.techradar.com/news/canon-eos-r5-9-new-things-weve-learnt-about-the-full-frame-mirrorless-flagship',
-    publishedAt: '2020-07-13T10:19:00Z',
-  },
-];
+import FETCH_STATUS from '../../app/helper/fetchStatus';
 
-export default Articles;
+const initialState = {
+  articles: [],
+  status: FETCH_STATUS.status[0],
+  error: FETCH_STATUS.error,
+};
+
+export default initialState;
