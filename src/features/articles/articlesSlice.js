92cb74fc1b30c2ac3f5a22f1c57bc31607e95952
John_Lamy <johnclamy@gmail.com>
2021-04-29 10:57:51 +0100

    setup fetch

--- src/features/articles/articlesSlice.js
+++ src/features/articles/articlesSlice.js
@@ -2,6 +2,17 @@ import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
 import { client } from '../../api/client';
 
 import initialState from './initialState';
+import FETCH_STATUS from '../../app/helper/fetchStatus';
+
+export const fetchArticles = createAsyncThunk(
+  'articles/fetchArticles',
+  async () => {
+    const response = await client.get('/newsapi/articles');
+    return response.articles;
+  }
+);
+
+const [, loading, succeeded, failed] = FETCH_STATUS.status;
 
 const articlesSlice = createSlice({
   name: 'articles',
@@ -24,19 +35,26 @@ const articlesSlice = createSlice({
       },
     },
   },
+  extraReducers: {
+    [fetchArticles.pending]: (state) => {
+      state.status = loading;
+    },
+
+    [fetchArticles.fulfilled]: (state, action) => {
+      state.status = succeeded;
+      state.articles = state.articles.concat(action.payload);
+    },
+
+    [fetchArticles.rejected]: (state, action) => {
+      state.status = failed;
+      state.error = action.error.message;
+    },
+  },
 });
 
 export const { articleAdded } = articlesSlice.actions;
-export const selectAllArticles = (state) => state.articles.articleList;
+export const selectAllArticles = (state) => state.articles.articles;
 export const selectArticleById = (state, articleId) =>
-  state.articles.articleList.find((a) => a.id === articleId);
-
-export const fetchArticles = createAsyncThunk(
-  'articles/fetchArticles',
-  async () => {
-    const response = await client.get('/newsapi/articles');
-    return response.articles;
-  }
-);
+  state.articles.articles.find((a) => a.id === articleId);
 
 export default articlesSlice.reducer;
